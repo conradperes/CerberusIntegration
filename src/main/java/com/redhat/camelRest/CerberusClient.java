2e80c7c6c191b184d695a1dce9e64c30d3ee7078
https://github.com/google/guava.wiki.git <conrad.marques.peres@gmail.com>
2017-03-29 17:46:11 -0300

    Cerberus Integration

---
+++ src/main/java/com/redhat/camelRest/CerberusClient.java
@@ -0,0 +1,25 @@
+package com.redhat.camelRest;
+
+public class CerberusClient {
+//	public static void main(String[] args) {
+//		HttpClient client = HttpClientBuilder.create().build();
+//		HttpGet request = new HttpGet("http://jeap.rio.rj.gov.br/cerberus/seam/resource/v1/permissoes");
+//		request.addHeader("ambiente", "DESENVOLVIMENTO");
+//		request.addHeader("provedor", "EXEMPLO");
+//		request.addHeader("consumidor", "EXEMPLO");
+//		request.addHeader("chaveAcesso", "d4fec2a2-7f41-43bd-9c0c-c00afe8f4858");
+//		request.addHeader("usuario", "05520685789");
+//		request.addHeader("senhaUsuario", "xxxx");
+//		HttpResponse response = null;
+//		try {
+//			response = client.execute(request);
+//
+//			InputStream inputStream = response.getEntity().getContent();
+//			String responseAsString = CharStreams.toString(new InputStreamReader(inputStream, "UTF-8"));
+//			System.out.println(response.getStatusLine().getStatusCode());
+//			System.out.println(responseAsString);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+}
